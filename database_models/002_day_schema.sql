ea1515370eab5374803b18ee40c0a975e924a77e
Yuyi-Hao <roshanisingh2354@gmail.com>
2025-12-06 16:17:09 +0530

    [DB] Database schema for trackers and diary entries

--- database_models/002_day_schema.sql
+++ database_models/002_day_schema.sql
@@ -1,5 +1,180 @@
 -- ========================
--- USER DAY
+-- CUSTOM TRACKERS DATA TABLE
+-- ========================
+DROP TABLE IF EXISTS custom_trackers_data;
+CREATE TABLE custom_trackers_data(
+    id INTEGER  PRIMARY KEY AUTOINCREMENT,
+    "user_id"   TEXT    NOT NULL CHECK(typeof("user_id") = "text"),
+    tracker_id  INTEGER NOT NULL,
+    date        DATE NOT NULL,
+
+    -- stored as TEXT for flexibility
+    value TEXT,
+
+    -- auto field
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE(user_id, tracker_id, date),
+    FOREIGN KEY (user_id) REFERENCES accounts(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (tracker_id) REFERENCES custom_trackers(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TRIGGER custom_trackers_data_updated
+AFTER UPDATE ON custom_trackers_data
+FOR EACH ROW
+WHEN NEW.modified_at <= OLD.modified_at
+BEGIN
+    UPDATE custom_trackers_data
+    SET modified_at = CURRENT_TIMESTAMP
+    WHERE id = NEW.id;
+END;
+
+-- ========================
+-- CUSTOM TRACKERS TABLE
+-- ========================
+DROP TABLE IF EXISTS custom_trackers;
+CREATE TABLE custom_trackers(
+    id INTEGER  PRIMARY KEY AUTOINCREMENT,
+    "user_id"      TEXT    NOT NULL CHECK(typeof("user_id") = "text"),
+    
+    name TEXT NOT NULL,
+    data_type TEXT NOT NULL CHECK(data_type IN (
+        'number', 'text', 'boolean', 'time', 'enum'
+    )),
+    unit TEXT,
+    enum_options TEXT,             -- JSON list for enum types ["Light","Medium","Hard"]
+    is_active BOOLEAN DEFAULT 1,   -- when off will no longer show in trackers
+
+    -- auto field
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE(user_id, name),
+    FOREIGN KEY (user_id) REFERENCES accounts(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TRIGGER custom_trackers_updated
+AFTER UPDATE ON custom_trackers
+FOR EACH ROW
+WHEN NEW.modified_at <= OLD.modified_at
+BEGIN
+    UPDATE custom_trackers
+    SET modified_at = CURRENT_TIMESTAMP
+    WHERE id = NEW.id;
+END;
+
+-- ========================
+-- WORK TRACKERS DATA TABLE
+-- ========================
+DROP TABLE IF EXISTS work_trackers_data;
+CREATE TABLE work_trackers_data(
+    id INTEGER  PRIMARY KEY AUTOINCREMENT,
+    "user_id"      TEXT    NOT NULL CHECK(typeof("user_id") = "text"),
+    date  DATE NOT NULL UNIQUE,
+    
+    -- time fields in 24 hrs clock
+    commute_time TIME,
+    return_time TIME,
+    break_time TIME,
+
+    -- numeric fields
+    given_work INTEGER,
+    completed_work INTEGER,
+
+    -- enums
+    workload INTEGER CHECK (workload BETWEEN 0 AND 5),   -- 0=none, 1=Ehh, 2=ok, 3=usual, 4=tiring, 5=hectic
+
+    -- auto field
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE(user_id, date),
+    FOREIGN KEY (user_id) REFERENCES accounts(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TRIGGER work_trackers_data_updated
+AFTER UPDATE ON work_trackers_data
+FOR EACH ROW
+WHEN NEW.modified_at <= OLD.modified_at
+BEGIN
+    UPDATE work_trackers_data
+    SET modified_at = CURRENT_TIMESTAMP
+    WHERE id = NEW.id;
+END;
+
+-- ========================
+-- PERSONAL TRACKERS DATA TABLE
+-- ========================
+DROP TABLE IF EXISTS personal_trackers_data;
+CREATE TABLE personal_trackers_data(
+    id INTEGER  PRIMARY KEY AUTOINCREMENT,
+    "user_id"      TEXT    NOT NULL CHECK(typeof("user_id") = "text"),
+    date  DATE NOT NULL UNIQUE,
+    
+    -- time fields in 24 hrs clock
+    wakeup_time TIME,
+    sleep_time TIME,
+    screen_time TIME,
+
+    -- numeric fields
+    water_intake REAL,  -- in liters
+
+    -- enums
+    exercise INTEGER CHECK (exercise IN (0,1,2,3)),  -- 0=None, 1=Light, 2=Medium, 3=Heavy
+    outgoing INTEGER CHECK (outgoing IN (0,1,2,3)),  -- 0=None, 1=Essentials, 2=Walk, 3=Enjoy
+    mood     INTEGER CHECK (mood BETWEEN 0 AND 5),   -- 0=none, 1=bad, 2=not good, 3=neutral, 4=good, 5=amazing
+
+    -- auto field
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE(user_id, date),
+    FOREIGN KEY (user_id) REFERENCES accounts(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TRIGGER trigger_personal_trackers_data_updated
+AFTER UPDATE ON personal_trackers_data
+FOR EACH ROW
+WHEN NEW.modified_at <= OLD.modified_at
+BEGIN
+    UPDATE personal_trackers_data
+    SET modified_at = CURRENT_TIMESTAMP
+    WHERE id = NEW.id;
+END;
+
+-- ========================
+-- DAIRY ENTRY TABLE
+-- ========================
+DROP TABLE IF EXISTS diary_entry;
+CREATE TABLE diary_entry(
+    id INTEGER  PRIMARY KEY AUTOINCREMENT,
+    "user_id"      TEXT    NOT NULL CHECK(typeof("user_id") = "text"),
+    date  DATE NOT NULL UNIQUE,
+    short_note TEXT,
+    long_entry TEXT,
+    attachments TEXT NOT NULL DEFAULT '()',
+
+    -- auto field
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE(user_id, date),
+    FOREIGN KEY (user_id) REFERENCES accounts(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TRIGGER trigger_diary_entry_updated
+AFTER UPDATE ON diary_entry
+FOR EACH ROW
+WHEN NEW.modified_at <= OLD.modified_at
+BEGIN
+    UPDATE diary_entry
+    SET modified_at = CURRENT_TIMESTAMP
+    WHERE id = NEW.id;
+END;
+
+-- ========================
+-- USER DAY TABLE
 -- ========================
 DROP TABLE IF EXISTS user_day;
 CREATE TABLE user_day(
@@ -7,16 +182,22 @@ CREATE TABLE user_day(
     "user_id"      TEXT    NOT NULL CHECK(typeof("user_id") = "text"),
     date  DATE NOT NULL,
 
-    -- diary
-    short_note TEXT NOT NULL,
-    long_entry TEXT,
+    -- Trackers field
+    diary_entry_id INTEGER,
+    personal_trackers_id INTEGER,
+    work_trackers_id INTEGER,
+    custom_tracker_data_list_ids TEXT NOT NULL DEFAULT '()',
 
     -- auto field
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     
     UNIQUE(user_id, date),
-    FOREIGN KEY (user_id) REFERENCES accounts(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+    -- TODO: need to attach foreign keys
+    FOREIGN KEY (user_id) REFERENCES accounts(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (diary_entry_id) REFERENCES diary_entry(id) ON DELETE SET NULL,
+    FOREIGN KEY (personal_trackers_id) REFERENCES personal_trackers_data(id) ON DELETE SET NULL,
+    FOREIGN KEY (work_trackers_id) REFERENCES work_trackers_data(id) ON DELETE SET NULL
 );
 
 CREATE TRIGGER trigger_user_day_updated
@@ -28,126 +209,3 @@ BEGIN
     SET modified_at = CURRENT_TIMESTAMP
     WHERE id = NEW.id;
 END;
-
--- ========================
--- TRACKER CATEGORY
--- ========================
-DROP TABLE IF EXISTS tracker_category;
-CREATE TABLE tracker_category(
-    id INTEGER  PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
-
-    -- auto field
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TRIGGER trigger_tracker_category_updated
-AFTER UPDATE ON tracker_category
-FOR EACH ROW
-WHEN NEW.modified_at <= OLD.modified_at
-BEGIN
-    UPDATE tracker_category
-    SET modified_at = CURRENT_TIMESTAMP
-    WHERE id = NEW.id;
-END;
-
--- ========================
--- TRACKER TYPE
--- ========================
-DROP TABLE IF EXISTS tracker_type;
-CREATE TABLE tracker_type(
-    id INTEGER  PRIMARY KEY AUTOINCREMENT,
-    category_id INTEGER NOT NULL,
-
-    -- if user_id IS NULL → system tracker (basic/wellness)
-    -- if user_id NOT NULL → belongs to a specific user
-    "user_id"   TEXT CHECK("user_id" IS NULL OR typeof("user_id") = 'text'),
-
-    name TEXT NOT NULL,
-    data_type TEXT NOT NULL, -- "number", "text", "boolean", "time", "enum"
-    unit TEXT,
-    options TEXT,
-
-    is_system BOOLEAN DEFAULT FALSE,
-
-    -- auto field
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (user_id) REFERENCES accounts(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES tracker_category(id) ON DELETE NO ACTION
-);
-
-CREATE TRIGGER trigger_tracker_type_updated
-AFTER UPDATE ON tracker_type
-FOR EACH ROW
-WHEN NEW.modified_at <= OLD.modified_at
-BEGIN
-    UPDATE tracker_type
-    SET modified_at = CURRENT_TIMESTAMP
-    WHERE id = NEW.id;
-END;
-
-
-
--- ========================
--- TRACKER ENTRIES
--- ========================
-DROP TABLE IF EXISTS tracker_entries;
-CREATE TABLE tracker_entries(
-    id INTEGER  PRIMARY KEY AUTOINCREMENT,
-    user_day_id INTEGER NOT NULL,
-    tracker_type_id INTEGER NOT NULL,
-
-    -- only one of these is used depending on tracker type
-    value_number    REAL,
-    value_text      TEXT,
-    value_time      TEXT,     -- e.g. "07:30"
-    value_boolean   INTEGER,  -- 0/1
-    value_enum      TEXT,     -- must be one of tracker_type.options
-
-    -- auto field
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (user_day_id) REFERENCES user_day(id) ON DELETE CASCADE,
-    FOREIGN KEY (tracker_type_id) REFERENCES tracker_type(id)
-);
-
-CREATE TRIGGER trigger_tracker_entries_updated
-AFTER UPDATE ON tracker_entries
-FOR EACH ROW
-WHEN NEW.modified_at <= OLD.modified_at
-BEGIN
-    UPDATE tracker_entries
-    SET modified_at = CURRENT_TIMESTAMP
-    WHERE id = NEW.id;
-END;
-
-
--- ========================
--- DEFAULT CATEGORY DATA
--- ========================
-
-INSERT INTO tracker_category (name) VALUES
-("basic"),
-("work"),
-("personal"),
-("custom"),
-("incident");
-
-
--- ========================
--- DEFAULT SYSTEM TRACKERS
--- ========================
-
-INSERT INTO tracker_type (category_id, name, data_type, unit, is_system)
-VALUES
-((SELECT id FROM tracker_category WHERE name='basic'), 'Wakeup Time', 'time', NULL, 1),
-((SELECT id FROM tracker_category WHERE name='basic'), 'Sleep Time', 'time', NULL, 1),
-((SELECT id FROM tracker_category WHERE name='basic'), 'Water Intake', 'number', 'ml', 1),
-((SELECT id FROM tracker_category WHERE name='basic'), 'Screen Time', 'number', 'hours', 1),
-((SELECT id FROM tracker_category WHERE name='work'), 'Meetings', 'number', NULL, 1),
-((SELECT id FROM tracker_category WHERE name='work'), 'Commute Time', 'number', 'minutes', 1),
-((SELECT id FROM tracker_category WHERE name='personal'), 'Reading', 'number', 'minutes', 1);
